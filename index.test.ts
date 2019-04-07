9eb6f27894968f409f18e69599dce87832850f8e
minamibashi rearn <minamibashi@rearn.info>
2019-04-07 10:30:42 +0900

    Add test

--- index.test.ts
+++ index.test.ts
@@ -4,7 +4,9 @@ const rand = (): number => {
   return Math.floor(Math.random() * 0x100);
 };
 const keys = (new Uint8Array(8)).map(rand);
+const keys2 = (new Uint8Array(9)).map(rand);
 const u8array = (new Uint8Array(64)).map(rand);
+const u8array2 = (new Uint8Array(66)).map(rand);
 
 const des = new DES(keys);
 test('reverse', () => {
@@ -20,7 +22,7 @@ const ff = new Uint8Array([
 ]); // 255
 const ffDes = new Uint8Array([
   0x21, 0xec, 0xdb, 0xc1, 0xa8, 0x5a, 0xe0, 0xe2,
-]); // '!\xec\xdb\xc1\xa8Z\xe0\xe2'
+]);
 test('255', () => {
   expect(des1.encrypt(ff))
     .toMatchObject(ffDes);
@@ -29,4 +31,22 @@ test('255', () => {
 test('255 reverse', () => {
   expect(des1.decrypt(ffDes))
     .toMatchObject(ff);
+});
+
+test('encrypt failed', () => {
+  expect(() => {
+    des.encrypt(u8array2);
+  }).toThrowError();
+});
+
+test('decrypt failed', () => {
+  expect(() => {
+    des.decrypt(u8array2);
+  }).toThrowError();
+});
+
+test('new DES failed', () => {
+  expect(() => {
+    new DES(keys2);
+  }).toThrowError();
 });
